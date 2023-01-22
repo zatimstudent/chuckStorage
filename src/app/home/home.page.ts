7f30bfa4711404f207d7bfea3289db03aa469e2c
Radimec <radimec@radimeec-air.local>
2023-01-22 02:56:52 +0100

    add button moved to the r-bottom

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,36 +11,6 @@ import { HttpClient } from '@angular/common/http';
 
 export class HomePage {
 
-  listData: any[] = [];
-
-  constructor(private dataService: DataService, private http: HttpClient) {
-    this.loadData();
-  }
-
-  async loadData(){
-    this.dataService.getData().subscribe(res =>{
-      this.listData = res;
-    });
-  }
-
-  async addData(){
-    this.http.get('https://api.chucknorris.io/jokes/random').subscribe(data => {
-      this.listData.push(data);
-    });
-  }
-
-  async removeItem(index: number){
-    this.dataService.removeItem(index);
-    this.listData.splice(index,1);
-  }
-
-  getRandomColor() {
-    let letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-
+ constructor() {
+ }
 }
